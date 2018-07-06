1c538757eea0a06d3e138849cefe757378ec8e53
Terry Louwers <terry.louwers@fourtress.nl>
2018-07-06 11:07:50 +0200

    Add description where to add flag needed for stack overflow check.

--- HeapCheck/heap_check.c
+++ HeapCheck/heap_check.c
@@ -41,6 +41,37 @@
  *              }
  *          }
  *
+ * \note    To use the 'end_of_heap_overrun()', a modification in the
+ *          function '_sbrk()' needs to be made. For Atmel this is in the
+ *          file 'syscalls.c', around line 65. A flag needs to be added to
+ *          mark the end of the heap.
+ *
+ *          // Modified '_sbrk()' function:
+ *
+ *          extern caddr_t _sbrk(int incr)
+ *          {
+ *              static unsigned char *heap = NULL;
+ *              unsigned char *prev_heap;
+ *              int ramend = (int)&__ram_end__;
+ *
+ *              if (heap == NULL)
+ *              {
+ *                  heap = (unsigned char *)&_end;
+ *              }
+ *              prev_heap = heap;
+ *
+ *              if (((int)prev_heap + incr) > ramend)
+ *              {
+ *                  // Heap and stack collision
+ *                  return (caddr_t) -1;
+ *              }
+ *
+ *              heap += incr;
+ *              *((uint32_t*)((void*)heap)) = 0xFAFBFCFD;      // Mark end of heap to detect stack overflow
+ *
+ *              return (caddr_t) prev_heap;
+ *          }
+ *
  * \note    This code is not to be used 'as-is': be sure you know where the
  *          stack and heap are located in your project and modify the code to
  *          match these areas.
@@ -122,5 +153,5 @@ bool end_of_heap_overrun(void)
         return true;
     }
 
-    return ( *(uint32_t*)heap_end != HEAP_END_VALUE );
+    return (*((uint32_t*)((void*)heap_end)) != HEAP_END_VALUE );
 }
